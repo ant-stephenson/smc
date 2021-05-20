4e282b32245385fe7a83b983ba20da47e7355fcc
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-05-20 14:06:41 +0100

    Correct data generation.

--- scripts/test.R
+++ scripts/test.R
@@ -18,12 +18,14 @@ rho <- 0.95
 sigma <- 0.15
 
 Xt <- generate_SV_data(mu, rho, sigma, tmax)
-Yt <- as.matrix(rnorm(tmax+1, 0, 1) * exp(Xt))
-boot_sv <- Bootstrap_SV$new(data = Yt, mu = -0, sigma = 1, rho = 0.8)
+Yt <- as.matrix(rnorm(tmax, mean = 0, sd = sqrt(exp(Xt))))
+boot_sv <- Bootstrap_SV$new(data = Yt, mu = -1, sigma = 0.15, rho = 0.95)
 
-N <- 1000
+N <- 50000
 output <- bootstrap_filter(boot_sv, N, tmax)
-str(output)
 
-plot(Yt, type = "l")
+par(mfrow = c(1,2))
+plot(Xt, type = "l")
 lines(output$mx, col = "red")
+plot(1:500, output$ess, type = "l")
+rm(output)
