9feb13a881a3f0006cba8b9ae255b4f00a91481a
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-05-20 09:33:49 +0100

    Update variable names.

--- R/generateSVmodel.R
+++ R/generateSVmodel.R
@@ -1,21 +1,20 @@
-
 update_Xt <- function(mu, rho, Ut, Xs) {
   Xt <- rho * (Xs - mu) + Ut + mu
   return(Xt)
 }
 
-test_generate_SV_data <- function(mu, rho, sigma2, tt) {
-  U <- rnorm(tt, 0, sqrt(sigma2))
+test_generate_SV_data <- function(mu, rho, sigma, tmax) {
+  U <- rnorm(tmax, 0, sqrt(sigma))
   X0 <- exp(rnorm(1, 0, 1)) 
-  Xt <- rho^tt * X0 + U * rho^c((tt-1):0) + mu * (1-rho^tt)
+  Xt <- rho^tmax * X0 + U * rho^c((tmax-1):0) + mu * (1-rho^tmax)
   return(c(X0,Xt))
 }
 #' @export generate_SV_data
-generate_SV_data  <- function(mu, rho, sigma2, tt) {
-  U <- rnorm(tt, 0, sigma2)
+generate_SV_data  <- function(mu, rho, sigma, tmax) {
+  U <- rnorm(tmax, 0, sigma)
   X0 <- rnorm(1, 0, 1) 
   Xt <- c(X0)
-  for (t in 2:(tt+1)) {
+  for (t in 2:(tmax+1)) {
     Xt <- c(Xt, update_Xt(mu, rho, U[t-1], Xt[t-1]))
   }
   return(Xt)
