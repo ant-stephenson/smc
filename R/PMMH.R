8733db38bfbf64483313d8055e312985c840a372
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-05-24 17:02:10 +0100

    updates to smc^2

--- R/PMMH.R
+++ R/PMMH.R
@@ -1,33 +1,36 @@
 # Computation of L_T^N for r_PMMH
-full_lik <- function(fk_model, x, A, theta) {
-  # get N and T
-  dims <- dim(A)
-  N <- dims[1]
-  tmax <- dims[2]
+full_lik <- function(fk_model, x, A, theta, N, tn) {
+  if(is.null(A)) A <- matrix(rep(1:N, tn), nrow = tn, ncol = N, byrow = TRUE)
   # compute the likelihood
-  p1 <- sum(exp(fk_model$logG(t = 1, x = x[1, ], mean = theta))) / N
-  p2 <- sapply(1:tmax, function(t) {
-    sum(exp(fk_model$logG(t = A[t, ], x = x[t+1, ], mean = theta))) / N
-  })
-  lik <- p1 * prod(p2)
+  if (tn > 1) {
+    p1 <- sum(exp(fk_model$logG(1, x[1, ], theta))) / N
+    p2 <- sapply(2:tn, 
+                 function(t) sum(exp(fk_model$logG(t, x[t, A[t-1, ]], theta))) / N)
+    lik <- p1 * prod(p2)
+  } else {
+    lik <- sum(exp(fk_model$logG(1, x[1, ], theta))) / N
+  }
   return(lik)
 }
 
 # One step particle marginal Metropolis hastings
 pmmh_onestep <- function(fk_model, theta, x, A, sd_prop, sd_prior, ...) {
+  # get N and T
+  dims <- dim(x)
+  N <- dims[2]
+  tn <- dims[1] - 1
   # update theta with random walk proposal
   theta_prop <- theta + rnorm(1, mean = 0, sd = sd_prop)
   # update X and A with bootstrap filter
-  bs_result <- bootstrap_filter(fk_model = fk_model, N = N, tmax = tmax, ...)
+  if (tn > 0) bs_result <- bootstrap_filter(fk_model = fk_model, N = N, tmax = tn, ...)
+  else bs_result <- bootstrap_onestep(fk_model, N = N, ...)
   x_prop <- bs_result$x
   A_prop <- bs_result$A
-  # remove object to save memory
-  rm(list = bs_result, envir = .GlobalEnv)
   # compute acceptance probability
   r_num <- dnorm(x = theta_prop, mean = 0, sd = sd_prior) *
-    full_lik(fk_model, x_prop, A, theta_prop)
+    full_lik(fk_model, x_prop, A, theta_prop, N, tn)
   r_denom <- dnorm(x = theta, mean = 0, sd = sd_prior) *
-    full_lik(fk_model, x, A, theta)
+    full_lik(fk_model, x, A, theta, N, tn)
   r_pmmh <- r_num / r_denom
   if(runif(1) < r_pmmh) return(list(theta = theta_prop, x = x_prop, A = A_prop))
   else return(list(theta = theta, x = x, A = A))
@@ -46,11 +49,10 @@ smc_squared <- function(Yt, Nx, Nt, sigma, rho, sd_prior, sd_prop,
                                                          sigma = sigma, rho = rho))
   # initialise x
   xs <- array(NA, dim = c(tmax+1, Nt, Nx))
-  xs[1, , ] <- unlist(lapply(sv_models, function(x) x$sample_m0(N = Nx))) %>% 
-    matrix(ncol = Nx) %>% t()
+  xs[1, , ] <- t(matrix(unlist(lapply(sv_models, function(x) x$sample_m0(N = Nx))), 
+                        ncol = Nx))
   # initialise A
-  As <- array(NA, dim = c(tmax+1, Nt, tmax+1))
-  As[1, , ] <- matrix(1:(tmax+1), nrow = Nt, ncol = (tmax + 1), byrow = TRUE)
+  As <- array(NA, dim = c(tmax+1, Nt, Nx))
   # initialise weights
   wm <- matrix(NA, nrow = Nx, ncol = Nt) # w^m
   Wm <- matrix(NA, nrow = Nx, ncol = Nt) # W^m
@@ -68,25 +70,34 @@ smc_squared <- function(Yt, Nx, Nt, sigma, rho, sd_prior, sd_prop,
       # move particles through PMMH kernel
       for (s in 1:Nt) {
         pmmh_results <- pmmh_onestep(fk_model = sv_models[[s]], theta = thetas[t-1, ], 
-                                     x = xs[t-1, s, ], A = As[t-1, s, ], 
+                                     x = t(as.matrix(xs[1:(t-1), s, ])), 
+                                     A = ifelse(t > 2, t(as.matrix(As[1:(t-2), s, ])), 
+                                                NULL), 
                                      sd_prop = sd_prop, sd_prior = sd_prior, ...)
         thetas[t, ] <- pmmh_results$theta
         xs[t, s, ] <- pmmh_results$x
-        As[t, s, ] <- pmmh_results$A
+        As[t-1, s, ] <- pmmh_results$A
+        # update FK models - rearrange Yt according to A_t^m
+        sv_models <- lapply(1:Nt, 
+                            function(s) Bootstrap_SV$new(data = Yt, 
+                                                         mu = thetas[t, s], 
+                                                         sigma = sigma, rho = rho))
       }
       wm[t-1, ] <- 1
     } else {
       thetas[t, ] <- thetas[t-1, ]
+      if (t == 2) {
+        # make an initial matrix for ancestor variables
+        As[t-1, , ] <- matrix(rep(1:Nx, tmax), nrow = tmax, ncol = Nx, byrow = TRUE)
+      } else {
+        As[t-1, , ] <- As[t-2, , ]
+      }
     }
-    # update FK models - rearrange Yt according to A_t^m
-    sv_models <- lapply(1:Nt, 
-                        function(s) Bootstrap_SV$new(data = as.matrix(Yt[As[t, s, ]]), 
-                                                     mu = thetas[t, s], 
-                                                     sigma = sigma, rho = rho))
     # resample x
-    xs[t, , ] <- unlist(lapply(sv_models, 
-                               function(x) x$sample_m(xp = xs[t-1, s, ]))) %>% 
-      matrix(ncol = Nx) %>% t()
+    xs[t, , ] <- t(matrix(unlist(lapply(1:Nt, 
+                                        function(s) Wm[t-1, s] * 
+                                          sv_models[[s]]$sample_m(xs[t-1, s, As[t-1, s, ]]))), 
+                          ncol = Nx))
     # update weights
     wm[t, ] <- wm[t-1, ] * 
       unlist(lapply(1:Nt, function(s) sum(exp(sv_models[[s]]$logG(t, xs[t, s, ]))) / Nx))
