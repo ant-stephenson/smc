f3b16b64e82811fde80a0529889da406445d2150
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-05-25 00:20:44 +0100

    little test for smc^2

--- scripts/test.R
+++ scripts/test.R
@@ -12,7 +12,7 @@ set.seed(1)
 # print(test_Xt)
 # print(Xt)
 
-tmax <- 250
+tmax <- 100
 mu <- -1
 rho <- 0.95
 sigma <- 0.15
@@ -22,7 +22,7 @@ Yt <- as.matrix(rnorm(tmax+1, mean = 0, sd = sqrt(exp(Xt))))
 
 boot_sv <- Bootstrap_SV$new(data = Yt, mu = -1, sigma = 0.15, rho = 0.95)
 
-N <- 5000
+N <- 10
 output <- bootstrap_filter(boot_sv, N, tmax)
 
 par(mfrow = c(2, 1))
@@ -32,3 +32,17 @@ lines(output$mx, col = "red")
 #plot(output$sdx, type = "l")
 plot(1:tmax, output$ess, type = "l")
 rm(output)
+
+tmax <- 1000
+mu <- -1
+rho <- 0.95
+sigma <- 0.15
+Xt <- generate_SV_data(mu, rho, sigma, tmax)
+Yt <- as.matrix(rnorm(tmax+1, mean = 0, sd = sqrt(exp(Xt))))
+Nx <- 100
+Nt <- 1000
+sd_prior <- 0.2
+mu_prior <- 0
+
+smc_results <- smc_squared(Yt, Nx, Nt, sigma, rho, 
+                           mu_prior = -1, sd_prior = 1, sd_prop = 1)
