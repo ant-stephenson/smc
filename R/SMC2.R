3b67facae9a2d49cee24a9a4cc53214f9ba40805
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-05-27 13:18:07 +0100

    revert to original single-parameter

--- R/SMC2.R
+++ R/SMC2.R
@@ -22,17 +22,14 @@ pmmh_onestep_rcpp <- function(fk_model, theta, x, A, mu_prior, sd_prior, sd_prop
   N <- dims[2]
   tn <- dims[1] - 1
   # update theta with random walk proposal
-  # theta_prop <- theta + rnorm(1, mean = 0, sd = sd_prop)
-  theta_prop <- c()
-  theta_prop[1] <- theta[1] + rnorm(1, mean = 0, sd = sd_prop[1])
-  theta_prop[2] <- exp(log(theta[2]) + rnorm(1, mean = 0, sd = sd_prop[2]))
+  theta_prop <- theta + rnorm(1, mean = 0, sd = sd_prop)
   # update X and A with boostrap filter
   if (tn > 0) bs_result <- bootstrap_filter_rcpp(fk_model, N, tn)
   else bs_result <- bootstrap_onestep_rcpp(fk_model, N)
   x_prop <- bs_result$x
   A_prop <- bs_result$A
   # compute acceptance probability
-  prior_diff <- sum(((theta - mu_prior)^2 - (theta_prop - mu_prior)^2) / (2 * sd_prior^2))
+  prior_diff <- ((theta - mu_prior)^2 - (theta_prop - mu_prior)^2) / (2 * sd_prior^2)
   lr_pmmh <- log_lik(fk_model, x_prop, A_prop, N, tn) - 
     log_lik(fk_model, x, A, N, tn) + prior_diff
   # accept
@@ -48,11 +45,10 @@ smc_squared_rcpp <- function(Yt, Nx, Nt, sigma, rho, mu_prior, sd_prior, sd_prop
   # compute threshold
   essmin <- essmin_fn(Nt)
   # initialise thetas
-  thetas <- array(NA, dim=c(tmax+1, Nt, 2))
-  thetas[1, ,1] <- rnorm(n = Nt, mean = mu_prior[1], sd = sd_prior[1])
-  thetas[1, ,2] <- exp(rnorm(n = Nt, mean = log(mu_prior[2]), sd = sd_prior[2]))
+  thetas <- matrix(NA, nrow = tmax+1, ncol = Nt)
+  thetas[1, ] <- rnorm(n = Nt, mean = mu_prior, sd = sd_prior)
   # initialise Nt FK models
-  sv_models <- lapply(1:Nt, function(s) new(Bootstrap_SV_C, Yt, thetas[1, s, 1], thetas[1, s, 2], rho))
+  sv_models <- lapply(1:Nt, function(s) new(Bootstrap_SV_C, Yt, thetas[1, s], sigma, rho))
   # initialise x
   xs <- array(NA, dim = c(tmax+1, Nt, Nx))
   xs[1, , ] <- matrix(unlist(lapply(sv_models, function(x) x$sample_m0(N = Nx))),
@@ -79,27 +75,22 @@ smc_squared_rcpp <- function(Yt, Nx, Nt, sigma, rho, mu_prior, sd_prior, sd_prop
         if (t == 2) A <- NULL
         else A <- matrix(As[1:(t-2), s, ], nrow = t-2)
         x <- matrix(xs[1:(t-1), s, ], nrow = t-1)
-        
-        # use population of thetas to improve sampling
-        mut <- sapply(1:2, function(k) sum(Wm[t-1,] * thetas[t-1, ,k])/sum(Wm[t-1,]))
-        sd_t <- sapply(1:2, function(k) sqrt(sum(Wm[t-1,] * (thetas[t-1,,k]-mut[k])^2)/sum(Wm[t-1,])))
-        
-        pmmh_results <- pmmh_onestep_rcpp(sv_models[[s]], thetas[t-1, s,], x, A,
-                                          mut, sd_prior, sd_t)
-        thetas[t, s,] <- pmmh_results$theta
+        pmmh_results <- pmmh_onestep_rcpp(sv_models[[s]], thetas[t-1, s], x, A,
+                                          mu_prior, sd_prior, sd_prop)
+        thetas[t, s] <- pmmh_results$theta
         xs[1:(t-1), s, ] <- pmmh_results$x
         if (!is.null(pmmh_results$A)) As[1:(t-2), s, ] <- pmmh_results$A
       }
       wm[t-1, ] <- 0 # log(1)
       # update Nt FK models
       sv_models <- lapply(1:Nt, 
-                          function(s) new(Bootstrap_SV_C, Yt, thetas[t, s, 1], thetas[t, s, 2], rho))
+                          function(s) new(Bootstrap_SV_C, Yt, thetas[t, s], sigma, rho))
     } else {
-      thetas[t, ,] <- thetas[t-1, ,]
+      thetas[t, ] <- thetas[t-1, ]
     }
     # update ancestor variables
     if (t > 2) As[t-1, , ] <- As[t-2, , ]
-    print(colMeans(thetas[t,,]))
+    print(mean(thetas[t, ]))
     # perform step t for each particle filter
     for (s in 1:Nt) {
       xs[t, s, ] <- sv_models[[s]]$sample_m(xs[t-1, s, As[t-1, s, ]])
